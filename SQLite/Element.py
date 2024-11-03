b638095e26b91e7d27dd1f5d5fc7dd4deccf6d92
cal-holiday <calhol@uw.edu>
2024-11-02 22:25:23 -0700

    Created SQLite file

---
+++ SQLite/Element.py
@@ -0,0 +1,6 @@
+from enum import Enum
+class Element(Enum):
+    EARTH = 1
+    FIRE = 2
+    AIR = 3
+    WATER = 4
--- SQLite/createSQLite.py
+++ SQLite/createSQLite.py
@@ -1,13 +1,13 @@
 import sqlite3
+from Element import Element
 con = sqlite3.connect("character.db")
 cur = con.cursor()
-cur.execute("""
-    CREATE TABLE monster(name, image,element, opposite, health, 
-    speed, basicAttack, specialAttack""")
-cur.execute("""
-    INSERT INTO monster VALUES
-        ()
-        ()
-        ()
-        ()
-    """)
+cur.execute("CREATE TABLE monster(name, image, element, opposite, health, speed, basicAttack, specialAttack)")
+data = [
+    ('Earth Monster', 'earth_monster.png', Element.EARTH.value, Element.AIR.value, 50, 20, 5, 10),
+    ('Fire Monster', 'fire_monster.png', Element.FIRE.value, Element.WATER.value, 30, 40, 5, 10),
+    ('Air Monster', 'air_monster.png', Element.AIR.value, Element.EARTH.value, 20, 50, 5, 10),
+    ('Water Monster', 'water_monster.png', Element.WATER.value, Element.FIRE.value, 40, 30, 5, 10)
+]
+cur.executemany("INSERT INTO monster VALUES(?, ?, ? ,?, ?, ?, ?, ?)", data)
+con.commit()
