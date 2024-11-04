478ed78e4fc2ff94f2e8c23034297d9cfde92f4a
cal-holiday <calhol@uw.edu>
2024-11-03 18:25:07 -0800

    fixed names

--- MonsterFactory.py
+++ MonsterFactory.py
@@ -3,8 +3,8 @@ from Element import Element
 from Monster import Monster
 class MonsterFactory():
 
-    def createMonster(theself, element):
-        element_val = element.value
+    def createMonster(theSelf, theElement):
+        element_val = theElement.value
         con = sqlite3.connect("character.db")
         cur = con.cursor()
         name = cur.execute("SELECT name FROM monster WHERE element=element_val")
@@ -12,7 +12,7 @@ class MonsterFactory():
         health = cur.execute("SELECT health FROM monster WHERE element=element_val")
         max_hp = health
         agility = cur.execute("SELECT agility FROM monster WHERE element=element_val")
-        element = element
+        element = theElement
         opposite = Element(cur.execute("SELECT opposite FROM monster WHERE element=element_val"))
         basic_attack = cur.execute("SELECT basicAttack FROM monster WHERE element=element_val")
         special_attack = cur.execute("SELECT specialAttack FROM monster WHERE element=element_val")
