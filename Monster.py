a240a26d4feb7c354207113360ff965f2cab9fd2
cal-holiday <calhol@uw.edu>
2024-11-05 12:54:06 -0800

    Removed files that should not be in main yet and moved them to a development branch

--- Monster.py
+++
@@ -1,9 +0,0 @@
-from CharacterInterface import CharacterInterface
-class Monster(CharacterInterface):
-    def __init__(theSelf, theName, theImage, theHealth, theMaxHP, theAgility, theElement, theOpposite, theBasicAttack,
-                 theMainAttack):
-        super().__init__(theName, theImage, theHealth, theMaxHP, theAgility, theElement, theOpposite, theBasicAttack,
-                         theMainAttack)
-
-
-
