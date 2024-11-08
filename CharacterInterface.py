e0351cc8a830c784dd8500d3e9bf861050ddd5e9
Eva Howard <7960.eva@gmail.com>
2024-11-07 23:42:06 -0800

    Update CharacterInterface.py
    fixed naming conventions

--- CharacterInterface.py
+++ CharacterInterface.py
@@ -29,50 +29,50 @@ class CharacterInterface(ABC):
     #setters
     def setName(self, name):
         if name is not None:
-            self.myName = name
+            self.name = name
         else:
             print("Name string is null")
 
     def setImage(self, image):
         if image is not None:
-            self.myImage = image
+            self.image = image
         else:
             print("Image string is null")
 
     def setMaxHP(self, maxHP):
         if maxHP > 0:
-            self.myMaxHP = maxHP
-            self.myHP = maxHP
+            self.maxHP = maxHP
+            self.HP = maxHP
         else:
             print("Max HP cannot be 0 or negative")
 
     def setAgility(self, agility):
         if agility > 0:
-            self.myAgility = agility
+            self.agility = agility
         else:
             print("Agility cannot be 0 or negative")
 
     def setElement(self, element):
         if isinstance(element, Enum):
-            self.myElement = element
+            self.element = element
         else:
             print(f"{element} is not an Enum.")
 
     def setOppositeElement(self, element):
         if isinstance(element, Enum):
-            self.myElement = element
+            self.opposite = element
         else:
             print(f"{element} is not an Enum.")
 
     def setAttack(self, attack):
         if attack > 0:
-            self.myAttack = attack
+            self.attack = attack
         else:
             print("Basic attack cannot be 0 or negative")
 
     def setSpecialAttack(self, specialAttack):
         if specialAttack > 0:
-            self.mySpecialAttack = specialAttack
+            self.specialAttack = specialAttack
         else:
             print("Main attack cannot be 0 or negative")
 
@@ -80,28 +80,28 @@ class CharacterInterface(ABC):
 
     #getter methods
     def getName(self):
-        return self.myName
+        return self.name
 
     def getImage(self):
-        return self.myImage
+        return self.image
 
     def getHealth(self):
-        return self.myHealth
+        return self.HP
 
     def getHP(self):
-        return self.myMaxHP
+        return self.maxHP
 
     def getAgility(self):
-        return self.myAgility
+        return self.agility
 
     def getElement(self):
-        return self.myElement
+        return self.element
 
     def getOppositeElement(self):
-        return self.myOppositeElement
+        return self.opposite
 
     def getBasicAttack(self):
-        return self.myBasicAttack
+        return self.attack
 
     def getMainAttack(self):
-        return self.myMainAttack
+        return self.specialAttack
