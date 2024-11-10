d1098661991c723e587fc3b4601b754acf00d8fe
cal-holiday <calhol@uw.edu>
2024-11-10 14:19:59 -0800

    Added methods for x,y coordinates of characters

--- CharacterInterface.py
+++ CharacterInterface.py
@@ -73,6 +73,18 @@ class CharacterInterface(ABC):
         else:
             print(f"{element} is not an Element.")
 
+    def set_x(self, x):
+        if isinstance(x, int):
+            self.x = x
+        else:
+            print(x, "is not an integer")
+
+    def set_y(self, y):
+        if isinstance(y, int):
+            self.y = y
+        else:
+            print(y, "is not an integer")
+
     """
     Getters return the values of the fields for a 
     character object
@@ -97,6 +109,12 @@ class CharacterInterface(ABC):
 
     def get_opposite_element(self):
         return self.element.get_opposite()
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
 
     """
     When a character uses its basic attack, it sends the character
