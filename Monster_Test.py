e156046cbfc19ac13027879778c4fcdd2f2af31f
2frogfrog <2frogfrog@gmail.com>
2024-11-14 16:55:50 -0800

    tested the constructor for monster

--- Monster_Test.py
+++ Monster_Test.py
@@ -5,11 +5,21 @@ from Element import Element
 
 
 class MyTestCase(unittest.TestCase):
+    monster = Monster("Gross", "image", 12, 12, Element.EARTH)
 
-    def test_something(self):
-        monster = Monster("Gross","image",12,12,Element.EARTH)
+    def test_monsterconstructor(self):
+        monster = Monster("Gross", "image", 12, 10, Element.EARTH)
+        self.assertEqual(monster.get_hp(),12) #tests hp
+        self.assertEqual(monster.get_image(), "image") #tests image
+        self.assertEqual(monster.get_max_hp(), 12) #tests max hp
+        self.assertEqual(monster.get_agility(), 10) #tests aglility
+        self.assertEqual(monster.get_element(), Element.EARTH) #tests element
 
-        self.assertEqual(monster.get_hp(),12)  # add assertion here
+    def test_heal(self):
+
+    #def test_specialattack(self):
+
+    #def test_attack(self):
 
 
 if __name__ == '__main__':
