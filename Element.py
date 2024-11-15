7fe0dace2161d0ce2deafaff97d8edc44bce0233
Eva Howard <7960.eva@gmail.com>
2024-11-14 17:52:59 -0800

    I hate this I'm just trying to test hero

--- Element.py
+++ Element.py
@@ -1,6 +1,18 @@
 from enum import Enum
+
 class Element(Enum):
     EARTH = 1
     FIRE = 2
     AIR = 3
     WATER = 4
+
+    @staticmethod
+    def get_opposite():
+        if Element.EARTH:
+            return Element.AIR
+        elif Element.FIRE:
+            return Element.WATER
+        elif Element.AIR:
+            return Element.EARTH
+        else:
+            return Element.FIRE
