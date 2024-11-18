5397a4e6b28dd774bbfcc404225853720a111d3b
Eva Howard <7960.eva@gmail.com>
2024-11-17 22:23:10 -0800

    testing potions

---
+++ test_Potion.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+
+class TestPotion(TestCase):
+
+    def test_set_image(self):
+        self.fail()
+
+    def test_get_image(self):
+        self.fail()
+
+    def test_drink(self):
+        self.fail()
