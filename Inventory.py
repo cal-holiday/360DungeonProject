5b336d35c052711ef7fb20c3c8fe4df3ac5970e5
2frogfrog <2frogfrog@gmail.com>
2024-11-17 23:48:48 -0800

    Started testing inventory and refactored the class as needed.

---
+++ Inventory.py
@@ -0,0 +1,41 @@
+import Pillar
+import Potion
+from Pillar import PolymorphismPillar, EncapsulationPillar, InheritancePillar, AbstractionPillar
+
+
+class Inventory:
+    def __init__(self):
+        self.health_potions = []
+        self.vision_potions = []
+        self.pillars = []
+        self.has_all_pillars = False
+
+
+    def add(self, object):
+        if object is not None:
+            if (object is isinstance(object, PolymorphismPillar)
+                    or (object is isinstance(object, EncapsulationPillar))
+                    or (object is isinstance(object, InheritancePillar))
+                    or object is isinstance(object, AbstractionPillar)):
+                self.pillars.append(object)
+                if len(self.pillars) > 3:
+                    self.has_all_pillars = True
+            elif object is isinstance(object, Potion.HealthPotion) or object is isinstance(object, Potion.VisionPotion):
+                if object.get_name() == "health_potion":
+                    self.health_potions.append(object)
+                else:
+                    self.vision_potions.append(object)
+        else:
+            print("Cannot add null object to inventory")
+
+
+    def drink_potion(self, potion):
+        if len(self.health_potions) > 0 and potion.get_name() == "health":
+            self.health_potions[0].drink()
+            del self.health_potions[0]
+        elif len(self.vision_potions) > 0:
+            self.vision_potions[0].drink()
+            del self.vision_potions[0]
+
+    def get_has_all_pillars(self):
+        return self.has_all_pillars
---
+++ test_inventory.py
@@ -0,0 +1,36 @@
+import unittest
+from Inventory import Inventory
+from Pillar import Pillar, PolymorphismPillar
+
+
+class MyTestCase(unittest.TestCase):
+    def test_inventory_constructor(self):
+        inventory = Inventory()
+        self.assertEqual(inventory.health_potions, [])
+        self.assertEqual(inventory.vision_potions,[])
+        self.assertEqual(inventory.pillars,[])
+        self.assertEqual(inventory.has_all_pillars,False)
+
+    #def test_inventory_add_health_potion(self):
+    #def test_inventory_add_vision_potion(self):
+    #def test_inventory_add_potion(self):
+    #def test_drink_health_potion(self):
+    #def test_drink_vision_potion(self):
+    def test_has_all_pillars_with_two_pillars(self):
+        inventory = Inventory()
+        pillar = PolymorphismPillar("image","hero")
+        inventory.add(pillar)
+        inventory.add(pillar)
+        self.assertEqual(inventory.has_all_pillars,False)
+
+    def test_all_pillars_with_all_pillars(self):
+        inventory = Inventory()
+        pillar = PolymorphismPillar("image", "hero")
+        inventory.add(pillar)
+        inventory.add(pillar)
+        inventory.add(pillar)
+        inventory.add(pillar)
+        self.assertEqual(inventory.has_all_pillars, True)
+
+if __name__ == '__main__':
+    unittest.main()
