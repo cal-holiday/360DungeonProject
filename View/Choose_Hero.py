772fe50951ea36452246bbbdd86507d945d4fe16
Eva Howard <7960.eva@gmail.com>
2024-11-21 18:05:21 -0800

    working on choose character screen

--- View/Choose_Hero.py
+++ View/Choose_Hero.py
@@ -1,9 +1,23 @@
 import pygame
-
+from sys import exit
 pygame.init()
 
-SCREEN_WIDTH = 900
-SCREEN_HEIGHT = 900
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Dungeon Adventure')
+font = pygame.font.SysFont("Times New Roman", 20)
+pygame.display.set_caption("Choose Your Hero")
+Clock = pygame.time.Clock()
+
+def draw_text(text, font, color, x, y):
+    img = font.render(text, True, color)
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    pygame.display.update()
+    Clock.tick(60)
