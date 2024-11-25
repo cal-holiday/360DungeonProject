27f4c6ac276fd30b5c68336275ca292da8dff521
2frogfrog <2frogfrog@gmail.com>
2024-11-25 12:29:24 -0800

    main menu work

--- Controller/main_menu_controller.py
+++ Controller/main_menu_controller.py
@@ -1,24 +1,27 @@
 from View import main_menu_view
 import pygame
 
-run = True
+def run():
+    run = True
+    while run:
 
-while run:
-    main_menu_view.screen.fill((234, 165, 108))
-    main_menu_view.draw_scaled_image("dungeonBackground.png", 0, 0, 810, 810)
-    main_menu_view.draw_scaled_image("banner.png", 155, 80, 500, 150)
-    main_menu_view.draw_header("Dungeon Adventure", 197, 120)
-    main_menu_view.draw_button("button.png", "New Game", 300, 300, 210, 50)
-    main_menu_view.draw_button("button.png", "Load Game", 300, 370, 210, 50)
-    main_menu_view.draw_button("button.png", "How to play", 300, 440, 210, 50)
-    quit_button = main_menu_view.draw_button("button.png", "Quit", 300, 510, 210, 50)
+        main_menu_view.screen.fill((234, 165, 108))
+        main_menu_view.draw_scaled_image("dungeonBackground.png", 0, 0, 810, 810)
+        main_menu_view.draw_scaled_image("banner.png", 155, 80, 500, 150)
+        main_menu_view.draw_header("Dungeon Adventure", 197, 120)
+        new_game = main_menu_view.draw_button("button.png", "New Game", 300, 300, 210, 50)
+        load_game = main_menu_view.draw_button("button.png", "Load Game", 300, 370, 210, 50)
+        rules = main_menu_view.draw_button("button.png", "How to play", 300, 440, 210, 50)
+        quit = main_menu_view.draw_button("button.png", "Quit", 300, 510, 210, 50)
 
-    if(quit_button == True):
-        run = False
+        if(quit):
+            run = False
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+        if(new_game):
+            pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
 
-    pygame.display.update()
+        pygame.display.update()
