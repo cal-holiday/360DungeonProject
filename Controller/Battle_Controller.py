c8a55cdb35ede5be97bfb9c0ba883bf5a385ce71
Eva Howard <7960.eva@gmail.com>
2024-11-26 12:24:32 -0800

    working on getting hero and monster to take turns in battle

--- Controller/Battle_Controller.py
+++ Controller/Battle_Controller.py
@@ -1,10 +1,12 @@
 from random import randint
 
 import pygame
+import time
 
 from Model.CharacterFactory import CharacterFactory
 from Model.Element import Element
 from Model.Hero import Hero
+from Model.Monster import Monster
 from View import Battle_View as View
 
 
@@ -16,47 +18,60 @@ def run(screen, monster):
     hero = Hero.get_instance()
     in_battle = True
 
+    black_rect = pygame.Rect(25, 475, 760, 190)
     white_rect = pygame.Rect(20, 470, 770, 200)
+
     health_rect = pygame.Rect(80, 20, 150, 25)
     health_rect_outline = pygame.Rect(80, 20, 150, 25)
 
     enemy_rect = pygame.Rect(370, 280, 150, 25)
     enemy_outline_rect = pygame.Rect(370, 280, 150, 25)
 
+    screen.fill((0, 0, 0))
+    View.draw_image("battle_background.jpg", 0, 0, 810, 450)
+    View.draw_image(hero.get_image(), 75, 330, 90, 90)
+    View.draw_image(monster.get_image(), 400, 330, 90, 90)
+    pygame.draw.rect(screen, (255, 255, 255), white_rect, 5)
+
     def update(character, text, damage):
-        View.draw_result(text, 40, 500)
+        pygame.draw.rect(screen, (0,0,0), black_rect)
         result = character.get_hp() + damage
         if result > 0:
+            View.draw_result(text, 40, 500)
             character.set_hp(character.get_hp() + damage)
-            print(character.get_name() + ": " + str(character.get_hp()))
-            monsters_turn()
+            if isinstance(character, Monster):
+                time.sleep(5)
+                print(character.get_name() + ": " + str(character.get_hp()))
+            else:
+                print(character.get_name() + ": " + str(character.get_hp()))
             return True
-        View.draw_result(character.get_name() + " won!", 40, 500)
+        else:
+            View.draw_result(character.get_name() + " won!", 40, 500)
         return False
 
     def monsters_turn():
         num = randint(1, 3)
+        hero_agility = hero.get_agility()
         if num == 1:
             result = monster.attack()
-            if result[0] > 5:
+            if result[0] > hero_agility:
                 in_battle = update(hero, hero.get_name() + " took " + str(result[1]) + " damage!", -result[1])
             else:
                 in_battle = update(hero, hero.get_name() + " dodged!", 0)
         elif num == 2:  # Special button
             result = hero.get_instance().special_attack()
-            if result[0] > 5 and monster.get_opposite_element() == monster.get_element():
+            if result[0] > hero_agility and monster.get_opposite_element() == monster.get_element():
                 in_battle = update(hero, hero.get_name() + " took " + str(result[1]) + " damage!", 2 * -result[1])
-            elif result[0] > 5:
+            elif result[0] > hero_agility:
                 in_battle = update(hero, hero.get_name() + " took " + str(result[1]) + " damage!", -result[1])
             else:
                 in_battle = update(hero, hero.get_name() + " dodged!", 0)
         else:
+            in_battle = update(monster,  monster.get_name() + " healed!", 0)
             monster.heal()
 
     while isRunning and in_battle:
         # Fill the screen and draw the background
-        screen.fill((0, 0, 0))
-        View.draw_image("battle_background.jpg", 0, 0, 810, 450)
 
         # Draw hero health bar and outline
         pygame.draw.rect(screen, (34, 139, 34), health_rect)
@@ -66,13 +81,9 @@ def run(screen, monster):
         View.draw_text("Agility:  " + str(hero.get_agility()), 20, 60)
 
         # Draw hero and monster images
-        View.draw_image(hero.get_image(), 75, 330, 90, 90)
-        View.draw_image(monster.get_image(), 400, 330, 90, 90)
         pygame.draw.rect(screen, (34, 139, 34), enemy_rect)
         pygame.draw.rect(screen, (255, 255, 255), enemy_outline_rect, 3)
 
-        # Draw a white border around the bottom area
-        pygame.draw.rect(screen, (255, 255, 255), white_rect, 5)
 
         # Draw buttons
         View.draw_button("battle_button.png", "Attack", 20, 700, 185, 75)
@@ -90,22 +101,24 @@ def run(screen, monster):
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:  # Left click
                     mx, my = event.pos
+                    monster_agility = monster.get_agility()
                     # Check if any buttons are clicked
                     if 20 <= mx <= 205 and 700 <= my <= 775:  # Attack button
                         result = hero.get_instance().attack()
-                        if result[0] > 5:
-                            in_battle = update(monster, "The monster took " + str(result[1]) + " damage!", -result[1])
+                        if result[0] > monster_agility:
+                            in_battle = update(monster,  monster.get_name() + " took " + str(result[1]) + " damage!", -result[1])
+
                         else:
                             in_battle = update(monster, "The monster dodged!", 0)
 
                     elif 215 <= mx <= 400 and 700 <= my <= 775:  # Special button
                         result = hero.get_instance().special_attack()
-                        if result[0] > 5 and monster.get_opposite_element() == monster.get_element():
-                            in_battle = update(monster, "The monster took " + str(result[1]) + " damage!", 2*-result[1])
-                        elif result[0] > 5:
-                            in_battle = update(monster, "The monster took " + str(result[1]) + " damage!", -result[1])
+                        if result[0] > monster_agility and monster.get_opposite_element() == monster.get_element():
+                            in_battle = update(monster,  monster.get_name() + " took " + str(result[1]) + " damage!", 2*-result[1])
+                        elif result[0] > monster_agility:
+                            in_battle = update(monster,  monster.get_name() + " took " + str(result[1]) + " damage!", -result[1])
                         else:
-                            in_battle = update(monster, "The monster dodged!", 0)
+                            in_battle = update(monster,  monster.get_name() + " dodged! ", 0)
 
                     elif 410 <= mx <= 595 and 700 <= my <= 775:  # Potion button
                         # Potion logic (if any)
@@ -114,6 +127,7 @@ def run(screen, monster):
                     elif 605 <= mx <= 790 and 700 <= my <= 775:  # Skip button
                         # Skip logic (if any)
                         pass
+                monsters_turn()
 
         # Update the display
         pygame.display.update()
