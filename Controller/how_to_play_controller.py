ed93e4dbc3369bdd9f7074c6dcfa7cfc15b477d7
2frogfrog <157554737+2frogfrog@users.noreply.github.com>
2024-12-09 17:09:43 -0800

    Add files via upload
    lol

---
+++ Controller/how_to_play_controller.py
@@ -0,0 +1,45 @@
+
+from View import how_to_play_view as help_view
+import pygame
+
+pygame.init()
+Clock = pygame.time.Clock()
+
+def run():
+    is_running = True
+    while is_running:
+
+
+        #images
+        help_view.draw_scaled_image("panel.png",0,0,help_view.SCREEN_WIDTH,help_view.SCREEN_HEIGHT)
+        help_view.draw_button("buttonSquare_beige.png","w", 500, 420, 75, 75)
+        help_view.draw_button("buttonSquare_beige.png","s",500, 500, 75, 75)
+        help_view.draw_button("buttonSquare_beige.png", "a", 420, 500, 75, 75)
+        help_view.draw_button("buttonSquare_beige.png", "d", 580, 500, 75, 75)
+
+        #texts
+        help_view.draw_header("HOW TO PLAY", 50, 50)
+        pygame.display.set_caption("How To Play")
+        help_view.draw_text("You are a hero trapped in a dungeon.",50,120)
+        help_view.draw_text("Navigate through the dungeon maze", 50, 160)
+        help_view.draw_text("to find the four pillars of OO.", 50, 200)
+        help_view.draw_text("Watch out for monsters! They can drop",50,240)
+        help_view.draw_text("pillars or potions to help you get", 50, 280)
+        help_view.draw_text("through the maze. Once you find all",50,320)
+        help_view.draw_text("four pillars you can find", 50, 360)
+        help_view.draw_text("the Exit and escape!", 50, 400)
+        help_view.draw_text("use wasd keys to move", 350, 600)
+
+
+
+        exit_button = help_view.draw_button("buttonSquare_beige.png","x",710,50,50,50)
+
+        if exit_button:
+            is_running = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+        pygame.display.update()
--- Controller/main_menu_controller.py
+++ Controller/main_menu_controller.py
@@ -1,24 +1,40 @@
 from View import main_menu_view
+from Controller import Choose_Hero_Controller
+from Controller import how_to_play_controller
+from Controller import you_died_controller
 import pygame
 
-run = True
+def run(theScreen):
+    is_running = True
+    screen = theScreen
+    while is_running:
+        main_menu_view.setScreen(screen)
+        main_menu_view.screen.fill((234, 165, 108))
+        main_menu_view.draw_scaled_image("dungeonBackground.png", 0, 0, 810, 810)
+        main_menu_view.draw_scaled_image("banner.png", 155, 80, 500, 150)
+        main_menu_view.draw_header("Dungeon Adventure", 197, 120)
+        pygame.display.set_caption("Main Menu")
+        new_game = main_menu_view.draw_button("button.png", "New Game", 300, 300, 210, 50)
+        load_game = main_menu_view.draw_button("button.png", "Load Game", 300, 370, 210, 50)
+        rules = main_menu_view.draw_button("button.png", "How to play", 300, 440, 210, 50)
+        quit_game = main_menu_view.draw_button("button.png", "Quit", 300, 510, 210, 50)
 
-while run:
-    main_menu_view.screen.fill((234, 165, 108))
-    main_menu_view.draw_scaled_image("dungeonBackground.png", 0, 0, 810, 810)
-    main_menu_view.draw_scaled_image("banner.png", 155, 80, 500, 150)
-    main_menu_view.draw_header("Dungeon Adventure", 197, 120)
-    main_menu_view.draw_button("button.png", "New Game", 300, 300, 210, 50)
-    main_menu_view.draw_button("button.png", "Load Game", 300, 370, 210, 50)
-    main_menu_view.draw_button("button.png", "How to play", 300, 440, 210, 50)
-    quit_button = main_menu_view.draw_button("button.png", "Quit", 300, 510, 210, 50)
+        if(quit_game):
+            is_running = False
 
-    if(quit_button == True):
-        run = False
+        if(new_game):
+            Choose_Hero_Controller.run(screen)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+        if(rules):
+            how_to_play_controller.run()
 
-    pygame.display.update()
+        if(load_game):
+            pass
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+        pygame.display.update()
+
