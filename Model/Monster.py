652be0fc48a7fbfdc77b63529eff66a015ff3eaf
Eva Howard <7960.eva@gmail.com>
2024-12-09 23:00:21 -0800

    working on monster dropping items

--- Model/Monster.py
+++ Model/Monster.py
@@ -1,10 +1,30 @@
 from random import randint
 
 from Model.CharacterInterface import CharacterInterface
+from Model.Pillar import AbstractionPillar
+from Model.Potion import HealthPotion
+
+
 class Monster(CharacterInterface):
     def __init__(self, name, image, hit_image, dead_image, max_hp, agility, element):
         super().__init__(name, image, hit_image, dead_image, max_hp, agility, element)
         self.hp = max_hp
+        self.has_items()
+
+    def has_items(self):
+        num1 = randint(1, 10)
+        num2 = randint(1, 10)
+        num3 = randint(1, 10)
+
+        if num1 > 4:
+            self.has_health_potion = True
+            #in battle at the end, checks if this is true and creates and adds a potion into inventory
+        if num2 > 4:
+            self.has_vision_potion = True
+        if num3 > 4:
+            if AbstractionPillar.__instance
+            #check if pillars are made and add one if possible to inventory at end of battle
+
 
     def attack(self):
         return super().attack()
