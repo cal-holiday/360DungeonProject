449d8f27ee64a69bc832f86d32c735da1049852c
Eva Howard <7960.eva@gmail.com>
2024-12-10 15:05:56 -0800

    pillar issues?

--- Controller/you_died_controller.py
+++ Controller/you_died_controller.py
@@ -4,7 +4,7 @@ from Model.Hero import Hero
 from View import you_died_view as you_died
 
 def run(theScreen):
-    #hero = Hero.get_instance()
+    hero = Hero.get_instance()
     is_running = True
     screen = theScreen
     while is_running:
@@ -13,7 +13,7 @@ def run(theScreen):
         you_died.draw_text("You Died :(",175,100)
         new_game = you_died.draw_button("button.png","New Game",300,500,200,75)
         quit_game = you_died.draw_button("button.png","Quit",300,600,200,75)
-        you_died.draw_rotated_image("air_hero_dead.png",350,300,100,100,90) #change to hero get instance once its all plugged together
+        you_died.draw_rotated_image(hero.get_dead_image(),350,300,100,100,90) #change to hero get instance once its all plugged together
 
         if (quit_game):
             is_running = False
