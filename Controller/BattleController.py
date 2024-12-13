a8aac570edf5a7b2d997902347bc4d9352ae75e7
cal-holiday <calhol@uw.edu>
2024-12-13 13:36:42 -0800

    Added comments

--- Controller/BattleController.py
+++ Controller/BattleController.py
@@ -2,21 +2,27 @@ import pygame
 import random
 
 from Controller import YouDiedController
-from Model.CharacterFactory import CharacterFactory
-from Model.Element import Element
 from Model.Hero import Hero
 from Model.Inventory import Inventory
 from Model.Monster import Monster
-from Model.Potion import HealthPotion
 from View import BattleView as View
 
 # Initialize pygame
 pygame.init()
 
+"""
+A method for running the battle screen.
+
+@param monster is the monster the hero is currently fighting
+"""
+
 def run(monster):
+    # Starts battle music
     pygame.mixer.init()
     pygame.mixer.music.load('Assets/dean town 10 12 24.wav')
     pygame.mixer.music.play(loops=-1)
+
+    # Pygame set up
     screen = pygame.display.set_mode((810, 810))
     isRunning = True
     clock = pygame.time.Clock()
@@ -28,6 +34,9 @@ def run(monster):
     black_rect = pygame.Rect(25, 475, 760, 190)
     white_rect = pygame.Rect(20, 470, 770, 200)
 
+    """
+    Updates the screen
+    """
     def redraw_screen():
         """Redraw the screen elements."""
         screen.fill((0, 0, 0))
@@ -37,6 +46,9 @@ def run(monster):
         View.draw_text(f"Health Potions: {inventory.number_of_health_potions()}", 470, 625)
         display_health_bars()
 
+    """
+    Updates the sprites, checks for damage and death scenarios
+    """
     def redraw_sprites(character, action):
         if action == "hit":
             if isinstance(character, Monster):
@@ -55,7 +67,13 @@ def run(monster):
         elif action == "idle":
             View.draw_image("Assets/" + hero.get_image(), 75, 330, 90, 90)
             View.draw_image("Assets/" + monster.get_image(), 400, 330, 90, 90)
-
+    """
+    Updates the screen based on damage and attack results
+    
+    @param character doing the action
+    @param text name of character being acted against
+    @param damage amount of damage done
+    """
     def update(character, text, damage):
         """Update health bars and display results."""
         result = character.get_hp() + damage
@@ -118,7 +136,9 @@ def run(monster):
         pygame.display.update()
         pygame.time.wait(action_delay)
         return True
-
+    """
+    Updates health bar
+    """
     def display_health_bars():
         """Draw health bars for the hero and monster."""
         # Hero health bar
@@ -140,6 +160,9 @@ def run(monster):
         pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(370, 270, 150, 25), 3)
         View.draw_text(f"{monster.get_hp()}/{monster.get_max_hp()}", 530, 263)
 
+    """
+    Handles the monsters turn by determining if the monster will use its basic attack, special attack, or heal
+    """
     def monsters_turn():
         """Monster's actions during its turn."""
         num = random.randint(1, 3)
@@ -165,13 +188,18 @@ def run(monster):
                 else:
                     return update(hero, f"{monster.get_name()} missed!", 0)
             return update(hero, f" {monster.get_name()} healed!", 0)
-
+    """
+    Handles the Hero's action based on the user's input. Available actions include basic attack, special attack,
+    use health potion, and skip. Skip is a cheat for testing/educational purposes.
+    """
     def hero_turn(action):
         """Handle hero's actions based on input."""
         monster_agility = monster.get_agility()
         if action == "attack":
             result = hero.attack()
-            if result[0] > monster_agility:
+            if result[0] > monster.get_agility() and monster.get_element() == hero.get_opposite_element():
+                return update(monster, f"{hero.get_name()} did {2 * result[1]} damage!", 2 * -result[1])
+            elif result[0] > monster_agility:
                 return update(monster, f"{hero.get_name()} did {result[1]} damage!", -result[1])
             else:
                 return update(monster, f"{hero.get_name()} missed!", 0)
@@ -188,11 +216,13 @@ def run(monster):
             inventory.drink_health_potion()
             return update(monster, f"{hero.get_name()} used a health potion!", 0)
 
+    """ Main game loop"""
     while isRunning and in_battle:
         redraw_screen()
         display_health_bars()
         redraw_sprites(hero, "idle")
 
+        #draw option buttons
         View.draw_button('Assets/battle_button.png', "Attack", 20, 700, 185, 75)
         View.draw_button('Assets/battle_button.png', "Special", 215, 700, 185, 75)
         View.draw_button('Assets/battle_button.png', "Potion", 410, 700, 185, 75)
@@ -205,6 +235,7 @@ def run(monster):
                 pygame.quit()
                 exit()
 
+            # determine user's input for Hero action
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 mx, my = event.pos
 
@@ -242,12 +273,3 @@ def run(monster):
 
         pygame.display.update()
         clock.tick(60)
-
-if __name__ == '__main__':
-    hero = CharacterFactory.create_hero("hero", Element.WATER)
-    screen = pygame.display.set_mode((810, 810))
-    monster = CharacterFactory.create_monster(Element.EARTH)
-    inventory = Inventory()
-    inventory.add(HealthPotion())
-    inventory.add(HealthPotion())
-    run(monster)
--- View/BattleView.py
+++ View/BattleView.py
@@ -88,22 +88,22 @@ def draw_rewards(monster):
     if monster.has_health_potion() and monster.has_vision_potion() and monster.has_pillar():
         draw_image('Assets/health_potion.png', three_items[0][0], three_items[0][1], 100, 100)
         draw_image('Assets/vision_potion.png', three_items[1][0], three_items[1][1], 100, 100)
-        draw_image(monster.get_pillar().get_image(), three_items[2][0], three_items[2][1], 100, 100)
+        draw_image("Assets/" + monster.get_pillar().get_image(), three_items[2][0], three_items[2][1], 100, 100)
     elif monster.has_health_potion() and monster.has_vision_potion():
         draw_image('Assets/health_potion.png', two_items[0][0], two_items[0][1], 100, 100)
         draw_image('Assets/vision_potion.png', two_items[1][0], two_items[1][1], 100, 100)
     elif monster.has_health_potion() and monster.has_pillar():
         draw_image('Assets/health_potion.png', two_items[0][0], two_items[0][1], 100, 100)
-        draw_image(monster.get_pillar().get_image(), two_items[1][0], two_items[1][1], 100, 100)
+        draw_image("Assets/" + monster.get_pillar().get_image(), two_items[1][0], two_items[1][1], 100, 100)
     elif monster.has_vision_potion() and monster.has_pillar():
         draw_image('Assets/vision_potion.png', two_items[0][0], two_items[0][1], 100, 100)
-        draw_image(monster.get_pillar().get_image(), two_items[1][0], two_items[1][1], 100, 100)
+        draw_image("Assets/" + monster.get_pillar().get_image(), two_items[1][0], two_items[1][1], 100, 100)
     elif monster.has_health_potion():
         draw_image('Assets/health_potion.png', one_item[0], one_item[1], 100, 100)
     elif monster.has_vision_potion():
         draw_image('Assets/vision_potion.png', one_item[0], one_item[1], 100, 100)
     elif monster.has_pillar():
-        draw_image(monster.get_pillar().get_image(), one_item[0], one_item[1], 100, 100)
+        draw_image("Assets/" + monster.get_pillar().get_image(), one_item[0], one_item[1], 100, 100)
 
     draw_button('Assets/button.png', "claim", 315, 500, 175, 70)
 
