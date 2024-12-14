57dbfd2f22fb1ed0b64b3042174e3691d34d69a9
cal-holiday <calhol@uw.edu>
2024-12-13 20:50:00 -0800

    Maze tests

--- Tests/TestMaze.py
+++ Tests/TestMaze.py
@@ -26,8 +26,7 @@ class TestMaze(unittest.TestCase):
 
     def test_delete_instance(self):
         Maze.delete_instance()
-        new_maze = Maze(self.maze_size)
-        self.assertNotEqual(self.maze, new_maze)
+        self.assertIsNone(Maze.get_instance())
 
     def test_maze_size_and_initialization(self):
         self.assertEqual(len(self.maze.get_array()), self.maze_size)
@@ -41,6 +40,24 @@ class TestMaze(unittest.TestCase):
         custom_array = [[Room(True, True, True, True, (x, y), None, None) for y in range(3)] for x in range(3)]
         self.maze.set_array(custom_array)
         self.assertEqual(self.maze.get_array(), custom_array)
+
+    def test_check_neighbor_none(self):
+        self.maze.set_array([[Room(True, True, True, True, (x, y), None, None) for y in range(3)] for x in range(3)])
+        for i in range(len(self.maze.get_array())):
+            for j in range(len(self.maze.room_array[i])):
+                self.maze.room_array[i][j].set_has_visited(True)
+        self.assertIsNone(self.maze.check_neighbors(self.maze.room_array[0][1]))
+
+    def test_check_neighbor(self):
+        self.maze.set_array([[Room(True, True, True, True, (x, y), None, None) for y in range(3)] for x in range(3)])
+        neighbors = [self.maze.room_array[0][1], self.maze.room_array[0][1], self.maze.room_array[1][2], self.maze.room_array[2][1]]
+        self.assertIn(self.maze.check_neighbors(self.maze.room_array[1][1]), neighbors)
+
+    def remove_north_wall(self):
+        self.maze.set_array([[Room(True, True, True, True, (x, y), None, None) for y in range(3)] for x in range(3)])
+        self.maze.remove_walls(self.maze.room_array[1][1], self.maze.room_array[0][1])
+        self.assertEqual(self.maze.room_array[1][1].get_nwall(), False)
+        self.assertEqual(self.maze.room_array[0][1].get_swall(), False)
 
 if __name__ == "__main__":
     unittest.main()
