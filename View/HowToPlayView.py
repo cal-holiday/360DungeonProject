7136b6a1e756970291dd863a1038f4e7acdf377e
cal-holiday <calhol@uw.edu>
2024-12-13 16:20:02 -0800

    Commented all view files

--- View/HowToPlayView.py
+++ View/HowToPlayView.py
@@ -1,51 +1,90 @@
 import pygame
 pygame.init()
 
-SCREEN_WIDTH = 810
-SCREEN_HEIGHT = 810
-FONT = 'Assets/8-bit-pusab.ttf'
+#Constants
 COLOR = (87,57,46)
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-font = pygame.font.Font(FONT, 20)
-header_font = pygame.font.Font(FONT,25)
+FONT = 'Assets/8-bit-pusab.ttf'
+HEADER_FONT = pygame.font.Font(FONT,25)
+SMALL_FONT = pygame.font.Font(FONT, 20)
 
-def draw_header(text, x,y):
-    img = header_font.render(text, True, COLOR)
-    screen.blit(img,(x,y))
+SCREEN_WIDTH = 810
+SCREEN_HEIGHT = 810
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
+"""
+A method for drawing headers.
+
+@param text the text to write out
+@param x the x coordinate of the top left corner of the text
+@param y the y coordinate of the top left corner of the text
+"""
+def draw_header(text, x, y):
+    img = HEADER_FONT.render(text, True, COLOR)
+    SCREEN.blit(img, (x, y))
+
+"""
+A method for drawing text.
+
+@param text the text to write out
+@param x the x coordinate of the top left corner of the text
+@param y the y coordinate of the top left corner of the text
+"""
 def draw_text(text, x, y):
-    img = font.render(text, True, COLOR)
-    screen.blit(img, (x,y))
+    img = SMALL_FONT.render(text, True, COLOR)
+    SCREEN.blit(img, (x, y))
 
+"""
+A method for drawing images.
+
+@param img the image to draw
+@param x the x coordinate of the top left corner of the image
+@param y the y coordinate of the top left corner of the image
+"""
 def draw_image(img, x, y):
-    screen.blit((pygame.image.load(img).convert()), (x,y))
+    SCREEN.blit((pygame.image.load(img).convert()), (x, y))
 
+"""
+A method for drawing scaled images.
+
+@param img the image to draw
+@param x the x coordinate of the top left corner of the image
+@param y the y coordinate of the top left corner of the image
+@param width how wide to make the image
+@param height how tall to make the image
+"""
 def draw_scaled_image(img, x, y, width, height):
     original_img = pygame.image.load(img).convert()
     scaled_img = pygame.transform.scale(original_img, (width, height))
-    screen.blit(scaled_img, (x, y))
+    SCREEN.blit(scaled_img, (x, y))
 
+"""
+A method for drawing buttons.
+
+@param img the button image
+@param text the text to add to the button
+@param x the x coordinate of the top left corner of the button
+@param y the y coordinate of the top left corner of the button
+@param width how wide to make the button
+@param height how tall to make the button
+"""
 def draw_button(img, text, x, y, width, height):
-    # Load the button image
     button_img = pygame.image.load(img).convert()
     scaled_img = pygame.transform.scale(button_img, (width, height))
     button_rect = scaled_img.get_rect(topleft=(x, y))
 
-    # Render the text
-    text_surface = font.render(text, True, COLOR)
+    text_surface = SMALL_FONT.render(text, True, COLOR)
     text_rect = text_surface.get_rect(center=button_rect.center)
 
-    # Draw the button
-    screen.blit(scaled_img, button_rect)
-    screen.blit(text_surface, text_rect)
+    SCREEN.blit(scaled_img, button_rect)
+    SCREEN.blit(text_surface, text_rect)
 
     # Check if the button is clicked
     mouse_pos = pygame.mouse.get_pos()
     mouse_click = pygame.mouse.get_pressed()
 
     if button_rect.collidepoint(mouse_pos) and mouse_click[0]:
-        return True  # Button is clicked
+        return True
 
     return False
--- View/YouWinView.py
+++ View/YouWinView.py
@@ -1,47 +1,88 @@
 import pygame
 pygame.init()
 
-FONT = 'Assets/8-bit-pusab.ttf'
+#Constants
 COLOR = (211,191,143)
 
-
-screen = pygame.display.set_mode((10,4))
-bigfont = pygame.font.Font(FONT, 40)
-font = pygame.font.Font(FONT, 20)
-
+FONT = 'Assets/8-bit-pusab.ttf'
+BIG_FONT = pygame.font.Font(FONT, 40)
+SMALL_FONT = pygame.font.Font(FONT, 20)
 
 Clock = pygame.time.Clock()
 
+"""
+A method for drawing text.
+
+@param screen the screen to draw the text on
+@param text the text to write out
+@param x the x coordinate of the top left corner of the text
+@param y the y coordinate of the top left corner of the text
+"""
 def draw_text(screen, text, x, y):
-    img = bigfont.render(text, True, COLOR)
+    img = BIG_FONT.render(text, True, COLOR)
     screen.blit(img, (x,y))
 
+"""
+A method for drawing images.
 
+@param screen the screen to draw the image on
+@param img the image to draw
+@param x the x coordinate of the top left corner of the image
+@param y the y coordinate of the top left corner of the image
+"""
 def draw_image(screen, img, x, y):
     screen.blit((pygame.image.load(img).convert()), (x,y))
 
+"""
+A method for drawing rotated images.
+
+@param screen the screen to draw the image on
+@param img the image to draw
+@param x the x coordinate of the top left corner of the image
+@param y the y coordinate of the top left corner of the image
+@param width how wide to make the image
+@param height how tall to make the image
+"""
 def draw_rotated_image(screen, img, x, y, width, height, degree):
     original_img = pygame.image.load(img).convert()
     scaled_img = pygame.transform.scale(original_img, (width, height))
     new_img = pygame.transform.rotate(scaled_img, degree)
     screen.blit(new_img, (x, y))
 
+"""
+A method for drawing scaled images.
+
+@param screen the screen to draw the image on
+@param img the image to draw
+@param x the x coordinate of the top left corner of the image
+@param y the y coordinate of the top left corner of the image
+@param width how wide to make the image
+@param height how tall to make the image
+"""
 def draw_scaled_image(screen, img, x, y, width, height):
     original_img = pygame.image.load(img).convert()
     scaled_img = pygame.transform.scale(original_img, (width, height))
     screen.blit(scaled_img, (x, y))
 
+"""
+A method for drawing buttons.
+
+@param screen the screen to draw the button on
+@param img the button image
+@param text the text to add to the button
+@param x the x coordinate of the top left corner of the button
+@param y the y coordinate of the top left corner of the button
+@param width how wide to make the button
+@param height how tall to make the button
+"""
 def draw_button(screen, img, text, x, y, width, height):
-    # Load the button image
     button_img = pygame.image.load(img).convert()
     scaled_img = pygame.transform.scale(button_img, (width, height))
     button_rect = scaled_img.get_rect(topleft=(x, y))
 
-    # Render the text
-    text_surface = font.render(text, True, COLOR)
+    text_surface = SMALL_FONT.render(text, True, COLOR)
     text_rect = text_surface.get_rect(center=button_rect.center)
 
-    # Draw the button
     screen.blit(scaled_img, button_rect)
     screen.blit(text_surface, text_rect)
 
@@ -50,7 +91,7 @@ def draw_button(screen, img, text, x, y, width, height):
     mouse_click = pygame.mouse.get_pressed()
 
     if button_rect.collidepoint(mouse_pos) and mouse_click[0]:
-        return True  # Button is clicked
+        return True
 
     return False
 
