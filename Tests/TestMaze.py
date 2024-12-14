c03173aedb8921465c669d82ae713c9ac31ab0ba
cal-holiday <calhol@uw.edu>
2024-12-13 21:06:38 -0800

    Maze tests almost done

--- Tests/TestMaze.py
+++ Tests/TestMaze.py
@@ -53,11 +53,80 @@ class TestMaze(unittest.TestCase):
         neighbors = [self.maze.room_array[0][1], self.maze.room_array[0][1], self.maze.room_array[1][2], self.maze.room_array[2][1]]
         self.assertIn(self.maze.check_neighbors(self.maze.room_array[1][1]), neighbors)
 
-    def remove_north_wall(self):
+    def test_remove_west_wall(self):
         self.maze.set_array([[Room(True, True, True, True, (x, y), None, None) for y in range(3)] for x in range(3)])
         self.maze.remove_walls(self.maze.room_array[1][1], self.maze.room_array[0][1])
+        self.assertEqual(self.maze.room_array[1][1].get_wwall(), False)
+        self.assertEqual(self.maze.room_array[0][1].get_ewall(), False)
+
+    def test_remove_east_wall(self):
+        self.maze.set_array([[Room(True, True, True, True, (x, y), None, None) for y in range(3)] for x in range(3)])
+        self.maze.remove_walls(self.maze.room_array[1][1], self.maze.room_array[2][1])
+        self.assertEqual(self.maze.room_array[1][1].get_ewall(), False)
+        self.assertEqual(self.maze.room_array[2][1].get_wwall(), False)
+
+    def test_remove_north_wall(self):
+        self.maze.set_array([[Room(True, True, True, True, (x, y), None, None) for y in range(3)] for x in range(3)])
+        self.maze.remove_walls(self.maze.room_array[1][1], self.maze.room_array[1][0])
         self.assertEqual(self.maze.room_array[1][1].get_nwall(), False)
-        self.assertEqual(self.maze.room_array[0][1].get_swall(), False)
+        self.assertEqual(self.maze.room_array[1][0].get_swall(), False)
+
+    def test_remove_south_wall(self):
+        self.maze.set_array([[Room(True, True, True, True, (x, y), None, None) for y in range(3)] for x in range(3)])
+        self.maze.remove_walls(self.maze.room_array[1][1], self.maze.room_array[1][2])
+        self.assertEqual(self.maze.room_array[1][1].get_swall(), False)
+        self.assertEqual(self.maze.room_array[1][2].get_nwall(), False)
+
+    def test_exit(self):
+        self.maze.set_array([[Room(True, True, True, True, (x, y), None, None) for y in range(3)] for x in range(3)])
+        self.maze.add_exit()
+        exit_flag = False
+        for i in range(len(self.maze.get_array())):
+            for j in range(len(self.maze.room_array[i])):
+                if self.maze.room_array[i][j].has_exit:
+                    exit_flag = True
+                    break
+        self.assertTrue(exit_flag)
+
+    def test_add_monster(self):
+        self.maze.set_array([[Room(True, True, True, True, (x, y), None, None) for y in range(3)] for x in range(3)])
+        self.maze.add_monsters()
+        monster_flag = False
+        for i in range(len(self.maze.get_array())):
+            for j in range(len(self.maze.room_array[i])):
+                if not self.maze.room_array[i][j].get_monster() is None:
+                    monster_flag = True
+                    break
+        self.assertTrue(monster_flag)
+
+    def test_add_health_potions(self):
+        self.maze.set_array([[Room(True, True, True, True, (x, y), None, None) for y in range(3)] for x in range(3)])
+        self.maze.add_health_potions()
+        potion_flag = False
+        potion = None
+        for i in range(len(self.maze.get_array())):
+            for j in range(len(self.maze.room_array[i])):
+                if not self.maze.room_array[i][j].potion is None:
+                    potion_flag = True
+                    potion = self.maze.room_array[i][j].potion
+                    break
+        self.assertTrue(potion_flag)
+        self.assertIsInstance(potion, HealthPotion)
+
+    def test_add_vision_potions(self):
+        self.maze.set_array([[Room(True, True, True, True, (x, y), None, None) for y in range(3)] for x in range(3)])
+        self.maze.add_vision_potions()
+        potion_flag = False
+        potion = None
+        for i in range(len(self.maze.get_array())):
+            for j in range(len(self.maze.room_array[i])):
+                if not self.maze.room_array[i][j].potion is None:
+                    potion_flag = True
+                    potion = self.maze.room_array[i][j].potion
+                    break
+        self.assertTrue(potion_flag)
+        self.assertIsInstance(potion, VisionPotion)
+
 
 if __name__ == "__main__":
     unittest.main()
